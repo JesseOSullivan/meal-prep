0879e0ecb9c882b52e4fec29e02d9b6cd187f841
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 14:29:39 +1000

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,7 @@
 export default async (request) => {
     const test = {test:"test"};
 
-    return new Response(test, {
+    return new Response(JSON.stringify(test), {
         headers: { "content-type": "application/json" },
     });
 };
