8c41a57174b65e368f551e07644a5d0db3ba0d56
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 16:06:32 +1000

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,28 +1,3 @@
-const gptFetch = async ({ req }) => {
-    const api_key = process.env.OPENAI_API_KEY;  // Replace with your API key
-
-    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${api_key}`
-        },
-        body: JSON.stringify({
-            prompt: req,
-            max_tokens: 60,
-        }),
-    });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log(data.choices[0].text);
-
-    return data.choices[0].text;
-};
-
 
 
 
@@ -31,6 +6,33 @@ export default async (request) => {
     const data = await request.json();  // Assuming the request has JSON payload
     const prompt = data.prompt;
 
+
+    const gptFetch = async ({ req }) => {
+        const api_key = process.env.OPENAI_API_KEY;  // Replace with your API key
+    
+        const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${api_key}`
+            },
+            body: JSON.stringify({
+                prompt: req,
+                max_tokens: 60,
+            }),
+        });
+    
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    
+        const data = await response.json();
+        console.log(data.choices[0].text);
+    
+        return data.choices[0].text;
+    };
+    
+    
 
     const result = gptFetch({req:data}) 
     
