56377ab0fc02328ceea2f8cd16dacf82b4446724
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 16:02:38 +1000

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,16 +1,13 @@
 export default async (request) => {
-    const { gptFetch } = require('../functions/utils/gpt');
     const data = await request.json();  // Assuming the request has JSON payload
     const prompt = data.prompt;
 
 
     // get products 
     
-    await gptFetch({req:"testing"})
     
     // switch to products
 
-    const response = { result: `Processed: ${prompt}` };  // Example response
 
     return new Response(JSON.stringify(data), {
         headers: { 'Content-Type': 'application/json' },
