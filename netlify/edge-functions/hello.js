452069cab5354c4aa9418b55b44e6ac8f0521051
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 14:37:31 +1000

    test endpoint

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,18 @@
-export default async (request) => {
-    const test = {test:"test"};
+const fetch = require('node-fetch');
 
-    return new Response(JSON.stringify(test), {
-        headers: { "content-type": "application/json" },
+
+export default async (request) => {
+    const data = await request.json();  // Assuming the request has JSON payload
+    const prompt = data.prompt;
+
+    
+    
+
+    const response = { result: `Processed: ${prompt}` };  // Example response
+
+    return new Response(JSON.stringify(response), {
+        headers: { 'Content-Type': 'application/json' },
     });
 };
+
+export const config = { path: "/test" };
