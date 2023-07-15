18a1fc5aa0350d95451f0b7b2f58e8f7538b4b4b
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 16:15:54 +1000

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,19 +1,20 @@
-export default async (request) => {
-    const { gptFetch } = require('./utils/gpt');
-    const data = await request.json();  // Assuming the request has JSON payload
+import { gptFetch } from './utils/gpt';
+
+export async function handler(event) {
+    const data = JSON.parse(event.body);  // Assuming the request has JSON payload
     const prompt = data.prompt;
 
-
     // get products 
-    
-    await gptFetch({req:"testing"})
-    
+
+    await gptFetch({ req: "testing" });
+
     // switch to products
 
-
-    return new Response(JSON.stringify(data), {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data),
         headers: { 'Content-Type': 'application/json' },
-    });
-};
+    };
+}
 
 export const config = { path: "/test" };
