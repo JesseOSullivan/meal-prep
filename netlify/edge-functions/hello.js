fccd3dcc0715f1aee67b52de0e8918e99d2fec82
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 16:09:17 +1000

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,15 +1,10 @@
-
-
-
-
 export default async (request) => {
     const data = await request.json();  // Assuming the request has JSON payload
     const prompt = data.prompt;
 
-
     const gptFetch = async ({ req }) => {
-        const api_key = process.env.OPENAI_API_KEY;  // Replace with your API key
-    
+        const api_key = 'YOUR_OPENAI_API_KEY';  // Replace with your API key
+
         const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
             method: 'POST',
             headers: {
@@ -21,24 +16,18 @@ export default async (request) => {
                 max_tokens: 60,
             }),
         });
-    
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-    
+
         const data = await response.json();
         console.log(data.choices[0].text);
-    
+
         return data.choices[0].text;
     };
-    
-    
 
-    const result = gptFetch({req:data}) 
-    
-    
-    // switch to products
-
+    const result = await gptFetch({req:prompt}) // You need to use await here, since gptFetch is asynchronous
 
     return new Response(JSON.stringify(result), {
         headers: { 'Content-Type': 'application/json' },
