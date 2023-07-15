8f0eee194742825a32dbc91f315fc9cc99f5433e
Patrick <42367024+Patadam@users.noreply.github.com>
2023-07-15 12:57:39 +1000

    add chat route

---
+++ pages/chat.jsx
@@ -0,0 +1,16 @@
+
+ 
+export default function Planner({ children }) {
+//   const { data, error } = useSWR('/api/navigation', fetcher)
+ 
+//   if (error) return <div>Failed to load</div>
+//   if (!data) return <div>Loading...</div>
+ 
+  return (
+    <>
+      <div>
+        <p>hello</p>
+      </div>
+    </>
+  )
+}
